44f66208a2626e4184e225c2f308d07adef5ab5b
juanjo1989@gmail.com <juanjo1989@gmail.com>
2019-03-18 15:33:30 +0100

    Add Core Control

--- Tutorial.php
+++ Tutorial.php
@@ -9,6 +9,7 @@ class Tutorial
 	public $plural;
 	public $customFields;
 	public $nameSpace;
+	public $icon;
 
 	public function __construct($singular, $plural, $path, $customFields)
 	{
@@ -17,13 +18,13 @@ class Tutorial
 		$this->db = 'bs-tutorial';
 		$this->nameSpace = 'bs_tutorial';
 		$this->path = $path;
+		$this->icon = 'dashicons-book-alt';
 		$this->customFields = $customFields;
 	}
 
 	public static function getInstance($singular, $plural, $path, $customFields)
 	{
-		if (  !self::$instance instanceof self)
-		{
+		if (!self::$instance instanceof self) {
 			self::$instance = new self($singular, $plural, $path, $customFields);
 		}
 		return self::$instance;
--- plugin.php
+++ plugin.php
@@ -14,6 +14,20 @@
 if (!defined('ABSPATH')) {
 	exit;
 }
+if (!in_array('bs-core/plugin.php', apply_filters('active_plugins', get_option('active_plugins')))) {
+	add_action('admin_notices', function () {
+		global $pagenow;
+		if ($pagenow == "plugins.php") {
+			?>
+			<div id="updated" class="error notice is-dismissible">
+				<p> Puede que algunos plugins vean afectados su comportamiento y estilo debido a que no se ha instalado
+					la dependencia con el Plugin "BS-CORE" disponible gratuitamente en https://bonseo.es/plugins</p>
+			</div>
+			<?php
+		}
+	});
+	return;
+}
 
 /** MODEL CONFIGURATION **/
 require_once plugin_dir_path(__FILE__) . '/Tutorial.php';
@@ -56,6 +70,7 @@ function bs_tutorial_register_post_type()
 		"show_in_menu" => true,
 		"show_in_nav_menus" => true,
 		"exclude_from_search" => false,
+		'menu_icon' => $model->icon,
 		"capability_type" => "post",
 		"map_meta_cap" => true,
 		"hierarchical" => false,
