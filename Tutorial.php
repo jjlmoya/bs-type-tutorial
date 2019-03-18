a02c3ca36c94b0afffa4d70082db28c6a1562810
juanjo1989@gmail.com <juanjo1989@gmail.com>
2019-03-18 11:37:27 +0100

    Add Custom Post Type Courses & Generic Custom Fields

---
+++ Tutorial.php
@@ -0,0 +1,36 @@
+<?php
+
+class Tutorial
+{
+	private static $instance;
+	public $db;
+	public $singular;
+	public $path;
+	public $plural;
+	public $customFields;
+	public $nameSpace;
+
+	public function __construct($singular, $plural, $path, $customFields)
+	{
+		$this->singular = $singular;
+		$this->plural = $plural;
+		$this->db = 'bs-tutorial';
+		$this->nameSpace = 'bs_tutorial';
+		$this->path = $path;
+		$this->customFields = $customFields;
+	}
+
+	public static function getInstance($singular, $plural, $path, $customFields)
+	{
+		if (  !self::$instance instanceof self)
+		{
+			self::$instance = new self($singular, $plural, $path, $customFields);
+		}
+		return self::$instance;
+	}
+
+}
+
+
+
+
